e1a8626ee6dccca3f6ea5628eba1a2997ba7f7fb
Terence Parr <parrt@antlr.org>
2012-11-22 16:45:16 -0800

    add

---
+++ scripts/leftnav.js
@@ -0,0 +1,11 @@
+document.write("<div id='sidebar'>");
+document.write("<ul>");
+document.write("<li class='active'><a href='#'>Donec sed odio dui</a></li>");
+document.write("<li><a href='#'>Aenean lacinia bibendum</a></li>");
+document.write("<li><a href='#'>Nulla sed consectetur</a></li>");
+document.write("<li><a href='#'>Etiam porta sem malesuada</a></li>");
+document.write("<li><a href='#'>Magna mollis euismod</a></li>");
+document.write("<li><a href='#'>Nulla vitae elit libero</a></li>");
+document.write("<li><a href='#'>Vivamus sagittis lacus vel</a></li>");
+document.write("</ul>");
+document.write("</div>");
