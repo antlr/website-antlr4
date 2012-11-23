84eaf352b61f3f9aaeb02acc4a9aaa4b98100db5
Terence Parr <parrt@antlr.org>
2012-11-22 16:52:30 -0800

    rm

--- scripts/top-nav.js
+++
@@ -1,9 +0,0 @@
-	document.write("<nav><ul>");
-	document.write("<li><a href="#">Download<span><v4.0></span></a></li>");
-	document.write("<li><a href="#">ANTLRWorks</a></li>");
-	document.write("<li><a href="#">Book</a></li>");
-	document.write("<li><a href="#">Doc</a></li>");
-	document.write("<li><a href="#">Grammars</a></li>");
-	document.write("<li><a href="#">Support</a></li>");
-	document.write("<li><a href="#">Bugs</a></li>");
-	document.write("</ul></nav>");
--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,9 +1,9 @@
 document.write("<ul>");
-document.write("<li><a href='#'>Download<span>v4.0</span></a></li>\n");
-document.write("<li><a href='#'>ANTLRWorks</a></li> \n");
-document.write("<li><a href='#'>Book</a></li> \n");
-document.write("<li><a href='#'>Doc</a></li>\n");
-document.write("<li><a href='#'>About ANTLR</a></li>\n");
-document.write("<li><a href='#'>Support</a></li>  \n");
-document.write("<li><a href='#'>Bugs</a></li> \n");
+document.write("<li><a href='download.html'>Download<span>v4.0</span></a></li>\n");
+document.write("<li><a href='works.html'>ANTLRWorks</a></li> \n");
+document.write("<li><a href='http://pragprog.com/book/tpantlr2/the-definitive-antlr-4-reference'>Book</a></li> \n");
+document.write("<li><a href='http://www.antlr.org/wiki/display/ANTLR4/ANTLR+4+Documentation'>Doc</a></li>\n");
+document.write("<li><a href='about.html'>About ANTLR</a></li>\n");
+document.write("<li><a href='support.html'>Support</a></li>  \n");
+document.write("<li><a href='https://github.com/antlr/antlr4/issues'>Bugs</a></li> \n");
 document.write("</ul>\n");
