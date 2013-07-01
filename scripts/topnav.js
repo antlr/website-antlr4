d6a99f091334910cd8a5760319cd567ea36638ab
Terence Parr <parrt@antlr.org>
2013-06-30 19:26:43 -0700

    update for 4.1

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,5 +1,5 @@
 document.write("<ul>");
-document.write("<li><a href='download.html'>Download<span>v4.0</span></a></li>\n");
+document.write("<li><a href='download.html'>Download<span>v4.1</span></a></li>\n");
 document.write("<li><a href='http://tunnelvisionlabs.com/products/antlrworks'>ANTLRWorks</a></li> \n");
 document.write("<li><a href='http://pragprog.com/book/tpantlr2/the-definitive-antlr-4-reference'>Book</a></li> \n");
 document.write("<li><a href='http://www.antlr.org/wiki/display/ANTLR4/ANTLR+4+Documentation'>Doc</a></li>\n");
