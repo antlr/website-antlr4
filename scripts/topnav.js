a18c3a55cc4dc7fb0772316fb8041a624fbb3bda
Terence Parr <parrt@antlr.org>
2014-01-21 17:35:41 -0800

    shift wiki links

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -2,7 +2,7 @@ document.write("<ul>");
 document.write("<li><a href='/download.html'>Download<span>v4.1</span></a></li>\n");
 document.write("<li><a href='http://tunnelvisionlabs.com/products/antlrworks'>ANTLRWorks</a></li> \n");
 document.write("<li><a href='http://pragprog.com/book/tpantlr2/the-definitive-antlr-4-reference'>Book</a></li> \n");
-document.write("<li><a href='http://www.antlr.org/wiki/display/ANTLR4/ANTLR+4+Documentation'>Doc</a></li>\n");
+document.write("<li><a href='https://theantlrguy.atlassian.net/wiki/display/ANTLR4/ANTLR+4+Documentation'>Doc</a></li>\n");
 document.write("<li><a href='/about.html'>About ANTLR</a></li>\n");
 document.write("<li><a href='/support.html'>Support</a></li>  \n");
 document.write("<li><a href='https://github.com/antlr/antlr4/issues'>Bugs</a></li> \n");
