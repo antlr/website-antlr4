12644e8d1b227d874eb4d7e8150a935cc77511e5
Terence Parr <parrt@antlr.org>
2015-01-22 18:50:15 -0800

    send out 4.5

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,5 +1,5 @@
 document.write("<ul>");
-document.write("<li><a href='/download.html'>Download<span>v4.4</span></a></li>\n");
+document.write("<li><a href='/download.html'>Download<span>v4.5</span></a></li>\n");
 document.write("<li><a href='/tools.html'>Dev Tools</a></li> \n");
 document.write("<li><a href='http://pragprog.com/book/tpantlr2/the-definitive-antlr-4-reference'>Book</a></li> \n");
 document.write("<li><a href='https://theantlrguy.atlassian.net/wiki/display/ANTLR4/ANTLR+4+Documentation'>Doc</a></li>\n");
