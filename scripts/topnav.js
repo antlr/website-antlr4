e276c6ceff8a96fea232fdc69fcb33c54ea1d372
Terence Parr <parrt@antlr.org>
2022-09-04 14:51:34 -0700

    Update website, javadoc, jars to 4.11.1

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,5 +1,5 @@
 document.write("<ul>");
-document.write("<li><a href='/download.html'>Download<span>v4.11.0</span></a></li>\n");
+document.write("<li><a href='/download.html'>Download<span>v4.11.1</span></a></li>\n");
 document.write("<li><a href='/tools.html'>Dev Tools</a></li> \n");
 document.write("<li><a href='http://pragprog.com/book/tpantlr2/the-definitive-antlr-4-reference'>Book</a></li> \n");
 document.write("<li><a href='https://github.com/antlr/antlr4/blob/master/doc/index.md'>Doc</a></li>\n");
